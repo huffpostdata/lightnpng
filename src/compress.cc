0eb78235b83a5efed8d5dbd027d3cc068a04da9d
Adam Hooper <adam@adamhooper.com>
2016-10-08 18:48:29 -0400

    Use a (modified) miniz instead of zlib, for speed

--- src/compress.cc
+++ src/compress.cc
@@ -1,29 +1,29 @@
 #include "compress.h"
 
-#include <cstring>
-#include <zlib.h>
+#define MINIZ_NO_ZLIB_COMPATIBLE_NAMES
+#include "upstream/miniz.c"
 
 static const size_t MaxChunkSize = 64 * 1024;
 
 CompressResult
 compress(uint8_t* bytes, size_t len)
 {
-  z_stream stream;
-  stream.zalloc = Z_NULL;
-  stream.zfree = Z_NULL;
-  stream.opaque = Z_NULL;
+  mz_stream stream;
+  stream.zalloc = 0;
+  stream.zfree = 0;
+  stream.opaque = 0;
 
   CompressResult ret = { COMPRESS_OK, 0, std::forward_list<std::string>() };
 
-  int zerr = deflateInit2(
+  int zerr = mz_deflateInit2(
     &stream,
-    Z_BEST_SPEED,
-    Z_DEFLATED,
+    MZ_BEST_SPEED,
+    MZ_DEFLATED,
     15,           // windowBits: 32kb window
     9,            // memLevel: maximum memory => maximum speed
-    Z_RLE         // because zlib manual says it's fast and good for PNG
+    MZ_RLE        // because zlib manual says it's fast and good for PNG
   );
-  if (zerr != Z_OK) {
+  if (zerr != MZ_OK) {
     ret.status = COMPRESS_ENOMEM;
     return ret;
   }
@@ -37,7 +37,7 @@ compress(uint8_t* bytes, size_t len)
     stream.avail_out = MaxChunkSize;
     stream.next_out = chunk;
 
-    deflate(&stream, Z_FINISH); // cannot error
+    mz_deflate(&stream, MZ_FINISH); // cannot error
 
     size_t chunkSize = MaxChunkSize - stream.avail_out;
     ret.data.emplace_front(reinterpret_cast<char*>(chunk), chunkSize); // TODO catch out-of-mem here
